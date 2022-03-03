0ac0e1867e7f10cea855315f65ca1b8e2a5193ae
igorWebMASTER <igor_1917@hotmail.com>
2022-03-02 22:58:01 -0300

    chore: add first styled components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import  * as S from './styles';
+
+
 export const Main = () => {
   return (
-    <div className="main">
-      <h1>React avançado</h1>
-    </div>
+    <S.Wrapper>
+      <S.Logo  src="/img/logo.svg" alt="Imagem de um átomo e React Avançado escrito ao lado."/>
+      <S.Title>React avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e StyledComponents
+      </S.Description>
+      <S.Illustration src="/img/hero-illustration.svg" alt="Um Desenvolvedor de frente a tela com código" />
+    </S.Wrapper>
   );
 }
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  text-align:center;
+  justify-content: center;
+  align-items:center;
+`;
+
+export const Logo = styled.img`
+`
+
+export const Title = styled.h1`
+
+`
+
+export const Description = styled.h1`
+
+`
+
+export const Illustration = styled.img`
+
+`
